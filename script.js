1864e3a932be319811d45a26e0dd1c126d287c56
rileygoerg <rgoerg@usf.edu>
2024-06-14 14:49:13 -0400

    Updated comments for readability

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+// U14123683
 function main(){
-    d3.selectAll("body")
+    d3.selectAll("body") 
+    // adds a div element to the HTML body
         .append("div")
+        // when moused over, changes background to firebrick
         .on("mouseover", function(){
         d3.select(this)
             .style("background-color", "firebrick")
          })
+         // when mouse leaves, reverts background to gray
          .on("mouseout", function(){
             d3.select(this)
             .style("background-color", "gray")
